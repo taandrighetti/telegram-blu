ed6de7283be5eb736f72f93973da25d9d6e54fd9
taandrighetti <taandrighetti@uc.cl>
2021-04-29 22:26:17 -0400

    Updates

---
+++ precache-manifest.9667484d399bdeba2d753fcd39c5aee6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba226dd97f6dba014fd779a780ec931",
+    "url": "/telegram-blu/index.html"
+  },
+  {
+    "revision": "80ed08c55a1ff0c15280",
+    "url": "/telegram-blu/static/css/0.52c23fd6.chunk.css"
+  },
+  {
+    "revision": "27b8418d887137d8d308",
+    "url": "/telegram-blu/static/css/3.12fbfec6.chunk.css"
+  },
+  {
+    "revision": "1802508f105d473d8d30",
+    "url": "/telegram-blu/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "a270c2617709f44aff64",
+    "url": "/telegram-blu/static/css/7.792a87e0.chunk.css"
+  },
+  {
+    "revision": "09655c15ebe5687cabc8",
+    "url": "/telegram-blu/static/css/main.bb4af673.chunk.css"
+  },
+  {
+    "revision": "80ed08c55a1ff0c15280",
+    "url": "/telegram-blu/static/js/0.864a42f9.chunk.js"
+  },
+  {
+    "revision": "27b8418d887137d8d308",
+    "url": "/telegram-blu/static/js/3.bb3dc8b3.chunk.js"
+  },
+  {
+    "revision": "fddd7f70ee082427e7f4",
+    "url": "/telegram-blu/static/js/4.831191ef.chunk.js"
+  },
+  {
+    "revision": "1802508f105d473d8d30",
+    "url": "/telegram-blu/static/js/5.d68c2e9d.chunk.js"
+  },
+  {
+    "revision": "43ec6daede3ea54d216b",
+    "url": "/telegram-blu/static/js/6.f4ba64a4.chunk.js"
+  },
+  {
+    "revision": "a270c2617709f44aff64",
+    "url": "/telegram-blu/static/js/7.1f4a60ba.chunk.js"
+  },
+  {
+    "revision": "09655c15ebe5687cabc8",
+    "url": "/telegram-blu/static/js/main.4ca5039c.chunk.js"
+  },
+  {
+    "revision": "514b29c1bf7cf6699f4d",
+    "url": "/telegram-blu/static/js/runtime~main.c28780ff.js"
+  },
+  {
+    "revision": "85479cc199de7ace1a44d901720601b8",
+    "url": "/telegram-blu/static/media/bubble-tail-left.85479cc1.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/telegram-blu/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "71da74694e314a0e3a855ded564a1eaf",
+    "url": "/telegram-blu/static/media/check.71da7469.svg"
+  },
+  {
+    "revision": "ac1331a490a116a48daa6c9f41b6db80",
+    "url": "/telegram-blu/static/media/telegram-logo.ac1331a4.svg"
+  }
+]);
